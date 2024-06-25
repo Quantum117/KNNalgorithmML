a96ea3b5d030c8552760046ddbbbb822c796f6cc
Iskandar <Iskandar@WIN-VO22OTKJS1B>
2024-06-26 01:11:09 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBoxTestData = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBoxDistance = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxIndex = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxClass = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.runToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1131, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(48, 24);
+            this.runToolStripMenuItem.Text = "Run";
+            this.runToolStripMenuItem.Click += new System.EventHandler(this.runToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 45);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(763, 221);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(844, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "K:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(869, 6);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(76, 22);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // textBoxTestData
+            // 
+            this.textBoxTestData.Location = new System.Drawing.Point(731, 8);
+            this.textBoxTestData.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxTestData.Name = "textBoxTestData";
+            this.textBoxTestData.Size = new System.Drawing.Size(89, 22);
+            this.textBoxTestData.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(572, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Test Data(x1,x2,x3,x4) :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Distance";
+            // 
+            // richTextBoxDistance
+            // 
+            this.richTextBoxDistance.Location = new System.Drawing.Point(53, 318);
+            this.richTextBoxDistance.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBoxDistance.Name = "richTextBoxDistance";
+            this.richTextBoxDistance.Size = new System.Drawing.Size(120, 114);
+            this.richTextBoxDistance.TabIndex = 7;
+            this.richTextBoxDistance.Text = "";
+            // 
+            // richTextBoxIndex
+            // 
+            this.richTextBoxIndex.Location = new System.Drawing.Point(260, 318);
+            this.richTextBoxIndex.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBoxIndex.Name = "richTextBoxIndex";
+            this.richTextBoxIndex.Size = new System.Drawing.Size(120, 114);
+            this.richTextBoxIndex.TabIndex = 8;
+            this.richTextBoxIndex.Text = "";
+            // 
+            // richTextBoxClass
+            // 
+            this.richTextBoxClass.Location = new System.Drawing.Point(468, 318);
+            this.richTextBoxClass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBoxClass.Name = "richTextBoxClass";
+            this.richTextBoxClass.Size = new System.Drawing.Size(120, 114);
+            this.richTextBoxClass.TabIndex = 9;
+            this.richTextBoxClass.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(256, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Index";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(472, 300);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Class";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 436);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 12, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1131, 26);
+            this.statusStrip1.TabIndex = 12;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(53, 20);
+            this.toolStripStatusLabel1.Text = "Ready.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 462);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBoxClass);
+            this.Controls.Add(this.richTextBoxIndex);
+            this.Controls.Add(this.richTextBoxDistance);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxTestData);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBoxTestData;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBoxDistance;
+        private System.Windows.Forms.RichTextBox richTextBoxIndex;
+        private System.Windows.Forms.RichTextBox richTextBoxClass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+using DataTable = Microsoft.Office.Interop.Excel.DataTable;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+
+        private string fileName;
+        private int k;
+        private List<double> testValues = new List<double>();
+        private HashSet<string> uniqueClasses = new HashSet<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double ParseToDouble(string value)
+        {
+            var culture = CultureInfo.InvariantCulture; // Use InvariantCulture for a consistent interpretation of decimal separators
+            if (double.TryParse(value, NumberStyles.Any, culture, out double result))
+            {
+                return result;
+            }
+            else
+            {
+                throw new FormatException($"Unable to parse '{value}' as double.");
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            k = Convert.ToInt32(numericUpDown1.Value);
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog { Filter = "Text Files|*.txt" })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = openFileDialog.FileName;
+                    toolStripStatusLabel1.Text = "Text file loaded.";
+                    LoadDataFromFile(fileName);
+                }
+            }
+        }
+
+        private void LoadDataFromFile(string filePath)
+        {
+            System.Data.DataTable dataTable = new System.Data.DataTable();
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                bool isFirstLine = true;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var parts = line.Split(' ');
+
+                    if (isFirstLine)
+                    {
+                        // Create columns for the DataTable
+                        for (int i = 0; i < parts.Length; i++)
+                        {
+                            dataTable.Columns.Add($"Column{i + 1}", typeof(string));
+                        }
+                        isFirstLine = false;
+                    }
+
+                    var newRow = dataTable.NewRow();
+                    for (int i = 0; i < parts.Length; i++)
+                    {
+                        newRow[i] = parts[i];
+                    }
+
+                    dataTable.Rows.Add(newRow);
+                }
+            }
+
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private double CalculateDistance(double[] testData, double[] dataPoint)
+        {
+            double distance = 0;
+            for (int i = 0; i < testData.Length; i++)
+            {
+                distance += Math.Pow((testData[i] - dataPoint[i]), 2);
+            }
+          distance = Math.Sqrt(distance);
+            return distance;
+        }
+
+        private void runToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string testDataString = textBoxTestData.Text;
+            var testData = testDataString.Split(' ').Select(double.Parse).ToArray();
+
+            if (testData.Length != 4)
+            {
+                MessageBox.Show("Invalid test data.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+           
+            string s="";
+            foreach(var i in testData )
+            {
+                s += i + " ";
+            }
+            MessageBox.Show(s);
+
+            if (testData.Length != 4) // Assumes 4 attributes in test data
+            {
+                MessageBox.Show("Invalid test data.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Get data from the DataGridView
+            var dataPoints = new List<double[]>();
+            var classData = new List<string>();
+
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                double[] dataRow = new double[4];
+                for (int j = 0; j < 4; j++)
+                {
+                    dataRow[j] = Convert.ToDouble(dataGridView1.Rows[i].Cells[j+1].Value);
+                }
+                dataPoints.Add(dataRow);
+                classData.Add(dataGridView1.Rows[i].Cells[0].Value.ToString());
+            }
+
+            // Calculate distances from the test data to each point in the dataset
+            var distances = dataPoints
+                .Select((point, index) => new
+                {
+                    Index = index,
+                    Distance = CalculateDistance(testData, point),
+                    Class = classData[index]
+                })
+                .OrderBy(item => item.Distance)
+                .Take(k) // Only take the top k distances
+                .ToArray();
+
+            // Display distances in the rich text boxes
+            richTextBoxDistance.Clear();
+            richTextBoxIndex.Clear();
+            richTextBoxClass.Clear();
+
+            foreach (var item in distances)
+            {
+                richTextBoxDistance.AppendText(item.Distance.ToString() + Environment.NewLine);
+                richTextBoxIndex.AppendText(item.Index.ToString() + Environment.NewLine);
+                richTextBoxClass.AppendText(item.Class + Environment.NewLine);
+            }
+
+            // Determine the most frequent class in the top k
+            var mostFrequentClass = distances
+                .GroupBy(d => d.Class)
+                .OrderByDescending(g => g.Count())
+                .First()
+                .Key;
+
+            toolStripStatusLabel1.Text = $"The class of test data is: {mostFrequentClass}";
+
+            if (k % 2 == 0)
+            {
+                MessageBox.Show("Value of K must be odd.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (distances.Count(d => d.Class == "Class1") > k / 2)
+            {
+                MessageBox.Show("Too many elements from Class1.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (distances.Count(d => d.Class == "Class2") > k / 2)
+            {
+                MessageBox.Show("Too many elements from Class2.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Handle cell content click events here
+        }
+    }
+}
